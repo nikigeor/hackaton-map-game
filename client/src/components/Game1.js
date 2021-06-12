5b4eff370f067582bbac3a4705eef2a7bf3dc2f2
fjjes <fjjesreen@gmail.com>
2021-06-12 14:24:23 -0600

    routes for minigames

---
+++ client/src/components/Game1.js
@@ -0,0 +1,9 @@
+const GameOne = () => {
+    return (
+        <div>
+            <h1>Game1 goes here...</h1>
+        </div>
+    );
+}
+ 
+export default GameOne;
---
+++ client/src/components/Game2.js
@@ -0,0 +1,9 @@
+const GameTwo = () => {
+    return (
+        <div>
+            <h1>Game2 goes here...</h1>
+        </div>
+    );
+}
+ 
+export default GameTwo;
