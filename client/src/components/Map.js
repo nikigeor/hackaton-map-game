9bfcaf02e45e70e49458616119b43d99f9a760bb
Christy <57055910+christyc01@users.noreply.github.com>
2021-06-12 11:43:59 -0600

    Set up routes, created placeholder component files

---
+++ client/src/components/Map.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { Icon } from "leaflet";
+import * as parkData from "../data/skateboard-parks.json";
+import 'leaflet/dist/leaflet.css';
+
+export const icon = new Icon({
+  iconUrl: "/star.png",
+  iconSize: [25, 25]
+});
+
+const Map = () => {
+  const [activePark, setActivePark] = React.useState(null);
+  return ( 
+    <div>
+      <h1>Our Beautiful Map</h1>
+      <MapContainer  id="mapid" center={[51.025, -114.1]} zoom={10}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+
+        {parkData.features.map(park => (
+          <Marker
+            key={park.properties.PARK_ID}
+            position={[
+              park.geometry.coordinates[1],
+              park.geometry.coordinates[0]
+            ]}
+            onClick={() => {
+              setActivePark(park);
+            }}
+            icon={icon}
+            >
+            <Popup>{park.properties.NAME}</Popup>
+          </Marker>
+        ))}
+
+        {activePark && (
+          <Popup
+            position={[
+              activePark.geometry.coordinates[1],
+              activePark.geometry.coordinates[0]
+            ]}
+            onClose={() => {
+              setActivePark(null);
+            }}
+          >
+            <div>
+              <h2>{activePark.properties.NAME}</h2>
+              <p>{activePark.properties.DESCRIPTIO}</p>
+            </div>
+          </Popup>
+        )}
+
+      </MapContainer>
+    </div>
+   );
+}
+ 
+export default Map;
